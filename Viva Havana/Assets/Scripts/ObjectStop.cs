3e49c3a231a8ea9fcd5a7872d1f781f22e520de2
Luga W <32984199+lujiawang@users.noreply.github.com>
2020-02-17 15:27:58 -0500

    Import project

---
+++ Viva Havana/Assets/Scripts/ObjectStop.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectStop : MonoBehaviour
+{
+    void OnCollisionEnter (Collision col)
+    {
+        Debug.Log("Hit " + col.gameObject.name);
+    }
+    
+    
+}
---
+++ Viva Havana/Assets/Skybox/DynamicSky.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Author: Lujia Wang */
+/* Animating the skybox*/
+
+public class DynamicSky : MonoBehaviour
+{
+
+    public float RotateSpeed = 1.2f;
+
+    public float SwitchTime = 5f;
+    //public float LerpSpeed = 1.2f;
+    public Material startMat;
+    public Material endMat;
+
+    void Start()
+    {
+        RenderSettings.skybox = startMat;
+    }
+
+
+    void Update()
+    {
+        //RenderSettings.skybox.Lerp(startMat, endMat, Time.time * LerpSpeed); 
+        RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotateSpeed);
+
+        //if(Time.time >= SwitchTime)
+        //{
+            //RenderSettings.skybox = endMat;
+        //}
+
+        //DynamicGI.UpdateEnvironment();
+    }
+}
