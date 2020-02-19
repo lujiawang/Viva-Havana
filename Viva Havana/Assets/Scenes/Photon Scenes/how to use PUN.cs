14880e1207230b946ce8f513c1f8cec46f7a2a91
Tzuyuj <zhou.2133@osu.edu>
2020-02-19 14:09:00 -0500

    some pun changes

--- Viva Havana/Assets/Scenes/Photon Scenes/how to use PUN.cs
+++ Viva Havana/Assets/Scenes/Photon Scenes/how to use PUN.cs
@@ -2,6 +2,8 @@
 // Open Window -> Photo Unity Networking -> PUN Wizard
 // Click Cloud Dashboard login
 // log in with our shared Gmail account and pw
-//copy the App id and paste into the box
+//copy the PUN number and 
+//click open set up project 
+//paste into the App id box
 //Connected !
 //Now run the Launcher.unity
