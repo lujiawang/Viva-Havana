f2f469486b51e047558d3fc5e3e801b90cdc2bf0
Luga W <32984199+lujiawang@users.noreply.github.com>
2020-02-19 14:54:21 -0500

    Finish the sky

--- Viva Havana/Assets/Scripts_justUS/DynamicSky.cs
+++ Viva Havana/Assets/Scripts_justUS/DynamicSky.cs
@@ -9,26 +9,66 @@ public class DynamicSky : MonoBehaviour
 {
 
     public float RotateSpeed = 1.2f;
-    public float ChangingSpeed = 1.0f;
-    
+    public float duration = 60f;
+        
     public Material skybox;
 
+
+    private float ChangingSpeed = 1.0f;
+
     private Color color;
-    private float red = 0.5f, green = 0.5f, blue = 0.5f;
+    private float red = 0.5f, green = 0.5f, blue = 0.5f, alpha = 0.5f;
+    private float redSpeed, greenSpeed, blueSpeed, alphaSpeed;
+    public float SunsetRed = 0.95f, SunsetGreen = 0.53f, SunsetBlue = 0.45f, SunsetAlpha = 0.3f;
+
+    private float exposure = 1.0f;
+    private float exposureSpeed;
 
     void Start()
-    {   //red = 0.5f, green = 0.5f, blue = 0.5f;
+    {
+        CalculateSpeed();
         RenderSettings.skybox = skybox;
         RenderSettings.skybox.SetFloat("_Exposure", 1.0f);
-        color = new Color(red,green,blue,0.5f);
+        color = new Color(red, green, blue, alpha);
+        RenderSettings.skybox.SetColor("_Tint", color);
     }
 
-
+    // default 30 fps
     void Update()
     {
-
-        RenderSettings.skybox.SetColor("_Tint", color);
+        //rotation
         RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotateSpeed);
+
+        //color
+        if (red <= SunsetRed && green <= SunsetGreen && blue >= SunsetBlue && alpha >= SunsetAlpha)
+        {
+            red -= redSpeed;
+            green -= greenSpeed;
+            blue -= blueSpeed;
+            alpha -= alphaSpeed;
+            color = new Color(red,green,blue,alpha);
+            RenderSettings.skybox.SetColor("_Tint", color);
+        }
+
+        //darkness
+        if (exposure >= 0.5f)
+        {
+            exposure -= exposureSpeed;
+            RenderSettings.skybox.SetFloat("_Exposure", exposure);
+        }
+    }
+
+    void CalculateSpeed()
+    {
+        float totalFrame = duration * 30f;
+
+        //color
+        redSpeed = (0.5f - SunsetRed) / totalFrame;
+        greenSpeed = (0.5f - SunsetGreen) / totalFrame;
+        blueSpeed = (0.5f - SunsetBlue) / totalFrame;
+        alphaSpeed = (0.5f - SunsetAlpha) / totalFrame;
         
+        //exposure
+        exposureSpeed = (1.0f - 0.5f) / totalFrame;
     }
 }
