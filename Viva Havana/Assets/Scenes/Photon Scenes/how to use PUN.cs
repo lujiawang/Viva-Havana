0ecd0f1d416a8545f6350a733a928da9c7133654
Tzuyuj <zhou.2133@osu.edu>
2020-02-19 16:34:51 -0500

    Revert "Merge branch 'master' of https://github.com/lujiawang/Viva-Havana"
    This reverts commit db49c27b74a8c3246484b869b686254b5317b38c, reversing
    changes made to f2f469486b51e047558d3fc5e3e801b90cdc2bf0.

--- Viva Havana/Assets/Scenes/Photon Scenes/how to use PUN.cs
+++ Viva Havana/Assets/Scenes/Photon Scenes/how to use PUN.cs
@@ -2,8 +2,6 @@
 // Open Window -> Photo Unity Networking -> PUN Wizard
 // Click Cloud Dashboard login
 // log in with our shared Gmail account and pw
-//copy the PUN number and 
-//click open set up project 
-//paste into the App id box
+//copy the App id and paste into the box
 //Connected !
 //Now run the Launcher.unity
