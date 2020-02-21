9be267d27c02b32592ed15b87f5847ff41d3d199
sherrylu971008 <60117478+sherrylu971008@users.noreply.github.com>
2020-02-21 16:29:49 -0500

    TV

---
+++ Viva Havana/Assets/SwapVideo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class SwapVideo : MonoBehaviour
+{
+	private VideoPlayer vp;
+
+	public VideoClip[] clips;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		vp = GetComponent<VideoPlayer>();
+		vp.clip = clips[0];
+		print(vp.clip.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
