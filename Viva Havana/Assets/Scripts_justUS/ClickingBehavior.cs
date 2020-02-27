0fd5e941c04d097ce43666887dcbc6f0615bd78c
Luga W <32984199+lujiawang@users.noreply.github.com>
2020-02-26 19:37:40 -0500

    merge

--- Viva Havana/Assets/Scripts_justUS/ClickingBehavior.cs
+++ Viva Havana/Assets/Scripts_justUS/ClickingBehavior.cs
@@ -2,12 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Passer;
+using UnityEngine.Events;
+using System;
 
 public class ClickingBehavior : MonoBehaviour
 {
-    public bool clicked = false;
-    public void Clicked()
+    private bool clicked = false;
+
+    public Transform fingerTipLeft;
+    public Transform fingerTipRight;
+
+    private float distance;
+    public float threshold;
+
+    public Transform[] buttons;
+    public UnityEvent[] OnClick;
+
+   
+
+    void Update()
     {
-        clicked = !clicked;
+        for (int i=0; i < buttons.Length; i++) { 
+
+            distance = Math.Min(Vector3.Distance(fingerTipLeft.position, buttons[i].position), Vector3.Distance(fingerTipRight.position, buttons[i].position));
+            if (distance <= threshold && !clicked)
+            {
+                clicked = true;
+                OnClick[i].Invoke();
+            }
+            else if (distance > threshold)
+            {
+                clicked = false;
+            }
+        }
+        
     }
 }
--- Viva Havana/Assets/Scripts_justUS/SwapVideo.cs
+++ Viva Havana/Assets/Scripts_justUS/SwapVideo.cs
@@ -9,22 +9,23 @@ public class SwapVideo : MonoBehaviour
 	private VideoPlayer vp;
 
 	public VideoClip[] clips;
-	private int index;
+	private int index = 0;
 
     // Start is called before the first frame update
     void Start()
     {
 		vp = GetComponent<VideoPlayer>();
-		vp.clip = clips[0];
-
+		vp.clip = clips[index];
     }
 
     
     public void Swapping()
     {
+
 		index++;
 		index %= clips.Length;
 		vp = GetComponent<VideoPlayer>();
 		vp.clip = clips[index];
+        
     }
 }
