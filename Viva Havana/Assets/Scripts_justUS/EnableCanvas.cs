ed1280e3eb50b1e7f58654a148a14e40d5555d1a
Luga W <32984199+lujiawang@users.noreply.github.com>
2020-02-26 19:17:43 -0500

    stuffstuffstuff

---
+++ Viva Havana/Assets/Scripts_justUS/EnableCanvas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnableCanvas : MonoBehaviour
+{
+    public GameObject gameObject;
+    public bool active = false;
+    public AudioSource Audio;
+
+    public void Enable()
+    {
+        active = !active;
+        gameObject.SetActive(active);
+
+        if (active)
+            Audio.Play();
+    }
+}
