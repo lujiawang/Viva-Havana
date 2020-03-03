8290bd88f9ce9d020e76ab80f6465813aabeb0b8
Luga W <32984199+lujiawang@users.noreply.github.com>
2020-03-03 16:26:34 -0500

    restaurant wall papers, gallery w/ scripts

---
+++ Viva Havana/Assets/Scripts_justUS/FrameDescription.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameDescription : MonoBehaviour
+{
+    public GameObject Text;
+    private GameObject Avatar;
+
+    public float trigger = 10f;
+
+    private void Start()
+    {
+        Avatar = GameObject.FindGameObjectWithTag("Player");
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Vector3.Distance(Text.transform.position, Avatar.transform.position) < trigger)
+        {
+            Text.SetActive(true);
+        }
+    }
+}
