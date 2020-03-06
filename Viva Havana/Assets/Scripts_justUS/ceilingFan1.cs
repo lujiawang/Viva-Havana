9e199b606f356ef65f30a344f5a44fe92dab483a
Tzuyuj <zhou.2133@osu.edu>
2020-03-05 21:43:05 -0500

    chairs mesh

---
+++ Viva Havana/Assets/Scripts_justUS/ceilingFan1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ceilingFan1 : MonoBehaviour
+{
+
+    public GameObject fan;
+    public GameObject center;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fan = GameObject.Find("rotateFans");
+        center = GameObject.Find("fan1/center");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(center.transform.position, Vector3.up, 90*Time.deltaTime);
+    }
+}
---
+++ Viva Havana/Assets/Scripts_justUS/ceilingFan2.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ceilingFan2 : MonoBehaviour
+{
+
+    public GameObject fan;
+    public GameObject center;
+    // Start is called before the first frame update
+    void Start()
+    {
+        fan = GameObject.Find("rotateFans");
+        center = GameObject.Find("fan2/center");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.RotateAround(center.transform.position, Vector3.up, 90*Time.deltaTime);
+    }
+}
