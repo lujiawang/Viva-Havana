58c7c383a4c046a11da04a11940be26b03a0d30f
Lujia Wang <lujiawang22@gmail.com>
2020-03-20 00:20:18 -0400

    walk

---
+++ Viva Havana/Assets/Scripts_justUS/Walking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Walking : MonoBehaviour
+{
+    public float speed = 0.02f;
+    public float longestDistance = 30f;
+
+    private float pedometer = 0f;
+    void Update()
+    {
+        pedometer += speed;
+        transform.Translate(new Vector3(0f,0f,speed),Space.Self);
+
+        if (pedometer >= longestDistance)
+        {
+            transform.Rotate(new Vector3(0f,180f,0f));
+            pedometer = 0f;
+        }
+    }
+    
+}
