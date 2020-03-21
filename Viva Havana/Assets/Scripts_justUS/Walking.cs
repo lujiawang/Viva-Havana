ab93a37ca66fe30f8ac757fd1ea9d7aaeea2ef1b
Lujia Wang <lujiawang22@gmail.com>
2020-03-21 02:14:02 -0400

    add chars to cathedral, fix shopping

--- Viva Havana/Assets/Scripts_justUS/Walking.cs
+++ Viva Havana/Assets/Scripts_justUS/Walking.cs
@@ -2,20 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/**
+ * 
+ * Author: Lujia Wang
+ * This script simply control the moving straight behavior,
+ * which can be applied to walking animators, cars, etc.
+ * 
+ **/
+
 public class Walking : MonoBehaviour
 {
     public float speed = 0.02f;
     public float longestDistance = 30f;
-
     private float pedometer = 0f;
+
+    private float rotateX = 0f;
+    public float rotateY = 180f;
+    private float rotateZ = 0f; //Usually just rotate around y-axis
+
     void Update()
     {
-        pedometer += speed;
-        transform.Translate(new Vector3(0f,0f,speed),Space.Self);
+        pedometer += Mathf.Abs(speed);
+        transform.Translate(new Vector3(0f,0f,speed), Space.Self);
 
         if (pedometer >= longestDistance)
         {
-            transform.Rotate(new Vector3(0f,180f,0f));
+            transform.Rotate(new Vector3(rotateX,rotateY,rotateZ));
             pedometer = 0f;
         }
     }
