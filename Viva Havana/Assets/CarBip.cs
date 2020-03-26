024ff8f71939530cd59e8484b6e6753edb5a93b6
sherrylu971008 <60117478+sherrylu971008@users.noreply.github.com>
2020-03-25 21:45:45 -0400

    CarBIIIIIIPPPPPP

---
+++ Viva Havana/Assets/CarBip.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarBip : MonoBehaviour
+{
+
+	private GameObject player;
+	public AudioClip SoundToPlay;
+	public float Volume;
+	AudioSource audio;
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+		audio = GetComponent<AudioSource>();
+
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+		player = GameObject.FindWithTag("Player");
+		object source = GetComponent<AudioSource>();
+		if (Vector3.Distance(player.transform.position, GameObject.Find("Car").transform.position) <= 5)
+		{
+			audio.PlayOneShot(SoundToPlay, Volume);
+		}
+	}
+}
