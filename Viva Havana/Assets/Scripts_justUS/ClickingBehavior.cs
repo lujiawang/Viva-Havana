ee0d0f50322fbc664409cc3443c06dcaba3365ae
Lujia Wang <lujiawang22@gmail.com>
2020-04-08 13:19:00 -0400

    dialogue

--- Viva Havana/Assets/Scripts_justUS/ClickingBehavior.cs
+++ Viva Havana/Assets/Scripts_justUS/ClickingBehavior.cs
@@ -8,6 +8,7 @@ using System;
 public class ClickingBehavior : MonoBehaviour
 {
     private bool clicked = false;
+    private float time;
 
     public UnityEvent OnClick;
     private Color origin;
@@ -19,14 +20,29 @@ public class ClickingBehavior : MonoBehaviour
         origin = GetComponent<Renderer>().material.color;
         darker = new Color(origin.r - 0.1f, origin.g - 0.1f, origin.b - 0.1f);
         audio = GetComponent<AudioSource>();
+
+        time = 0;
+    }
+
+    void FixedUpdate()
+    {
+        time += Time.time;
+        if (time >= 5 && clicked)
+        {
+            time = 0;
+            clicked = false;
+        }
     }
     
     private void OnTriggerEnter(Collider other)
     {
         GetComponent<Renderer>().material.color = darker;
-        OnClick.Invoke();
-        print("clicked");
-        audio.Play();
+        if (!clicked)
+        {
+            OnClick.Invoke();
+            clicked = true;
+            audio.Play();
+        }
     }
 
     private void OnTriggerExit(Collider other)
