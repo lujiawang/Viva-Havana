830b6b879e927e9182c46e2518f7a57194528d06
Lujia Wang <lujiawang22@gmail.com>
2020-04-24 15:25:08 -0400

    credit

--- Viva Havana/Assets/Scripts_justUS/Quit.cs
+++ Viva Havana/Assets/Scripts_justUS/Quit.cs
@@ -4,8 +4,17 @@ using UnityEngine;
 
 public class Quit : MonoBehaviour
 {
+    public Canvas canvas;
+    void Start()
+    {
+        canvas.renderMode = RenderMode.ScreenSpaceCamera;
+        canvas.worldCamera = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+        
+    }
     public void QuitApp()
     {
         Application.Quit();
     }
+
+   
 }
